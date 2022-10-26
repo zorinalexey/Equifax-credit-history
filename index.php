872964a78f705969dc4e77ce645e1dcde240f546
Алексей Зорин <zorinalexey59292@gmail.com>
2022-10-26 13:35:41 +0400

    add .gitignore file

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-$client = new \Equifax\CreditHistory\Client();
-$client->documentTypes = 'Паспорт гражданина Российской Федерации';
-$client->typesOfCollateralAndNonMonetaryGrantsUnderTheTransaction = '3.10';
-print_r($client->getReport());
